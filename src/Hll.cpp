01f070eed51763eb52453660e2b4b5a3e0a4aa7a
Pawel Szostek <p.szostek@criteo.com>
2017-01-20 16:41:32 +0100

    factor out MurMurHash as a template function outside of Hll

--- src/Hll.cpp
+++ src/Hll.cpp
@@ -5,39 +5,6 @@
 
 #include "Hll.h"
 
-uint64_t Hll::murmurHash( const void * key, int len, unsigned int seed ) const
-{
-  const uint64_t m = 0xc6a4a7935bd1e995;
-  const int r = 47;
-  uint64_t h = seed ^ (len * m);
-  const uint64_t * data = (const uint64_t *)key;
-  const uint64_t * end = data + (len/8);
-  while(data != end)
-  {
-    uint64_t k = *data++;
-    k *= m;
-    k ^= k >> r;
-    k *= m;
-    h ^= k;
-    h *= m;
-  }
-  const unsigned char * data2 = (const unsigned char*)data;
-  switch(len & 7)
-  {
-    case 7: h ^= uint64_t(data2[6]) << 48;
-    case 6: h ^= uint64_t(data2[5]) << 40;
-    case 5: h ^= uint64_t(data2[4]) << 32;
-    case 4: h ^= uint64_t(data2[3]) << 24;
-    case 3: h ^= uint64_t(data2[2]) << 16;
-    case 2: h ^= uint64_t(data2[1]) << 8;
-    case 1: h ^= uint64_t(data2[0]);
-    h *= m;
-  };
-  h ^= h >> r;
-  h *= m;
-  h ^= h >> r;
-  return h;
-}
 
 uint64_t Hll::bucket(uint64_t hash) {
   // Get the most significant bits and shift that
@@ -95,7 +62,8 @@ Hll::~Hll() {
 }
 
 void Hll::add(uint64_t value) {
-  uint64_t hash = murmurHash(&value, 8, HASH_SEED);
+  MurMurHash<uint64_t> murmur;
+  uint64_t hash = murmur(value);
   // We store in the synopsis for the the biggest leftmost one
   synopsis[bucket(hash)] = std::max(synopsis[bucket(hash)], leftMostSetBit(hash));
 }
--- src/Hll.h
+++ src/Hll.h
@@ -8,6 +8,50 @@
 #ifndef _HLL_H_
 #define _HLL_H_
 
+template <typename T>
+class Hash {
+  public:
+    virtual uint64_t operator()(T value) const = 0;
+};
+
+template<typename T>
+class MurMurHash : public Hash<T> {};
+
+template<>
+class MurMurHash<uint64_t> : public Hash<uint64_t>{
+  public:
+    uint64_t operator()(uint64_t value) const override {
+      const uint64_t m = 0xc6a4a7935bd1e995;
+      unsigned int seed = HASH_SEED;
+      const int r = 47;
+      uint64_t h = seed ^ (sizeof(uint64_t) * m);
+      {
+        uint64_t k = value;
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+        h ^= k;
+        h *= m;
+      }
+      const unsigned char * data2 = (const unsigned char*)&value;
+      switch(sizeof(uint64_t) & 7)
+      {
+        case 7: h ^= uint64_t(data2[6]) << 48;
+        case 6: h ^= uint64_t(data2[5]) << 40;
+        case 5: h ^= uint64_t(data2[4]) << 32;
+        case 4: h ^= uint64_t(data2[3]) << 24;
+        case 3: h ^= uint64_t(data2[2]) << 16;
+        case 2: h ^= uint64_t(data2[1]) << 8;
+        case 1: h ^= uint64_t(data2[0]);
+        h *= m;
+      };
+      h ^= h >> r;
+      h *= m;
+      h ^= h >> r;
+      return h;
+    }
+};
+
 class Hll {
   private:
     uint8_t bucketBits;
