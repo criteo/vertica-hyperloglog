5c32af7ef1319378e657b17f72232f9c9dd0bb34
Pawel Szostek <p.szostek@criteo.com>
2017-02-08 11:58:18 +0100

    Fix tests for ser/des with 5 and 6 bits per bucket
    This commit improves the tests by checking the results
    for the precision spanning from 10 to 18. Before it was only one
    precision value per test.

--- tests/hll_test.cpp
+++ tests/hll_test.cpp
@@ -39,90 +39,120 @@ class HllTest : public ::testing::Test {
 };
 
 /**
+ *                **************
+ *                *** 6 BITS ***
+ *                **************
+ *
  * What we want to test is basically that the HLLHdr
  * get serialized as well. If so, the synopsis size will
  * be 8 bytes longer than for HllRaw
  */
 TEST_F(HllTest, TestSerializeDeserialize6Bits) {
-  Hll<uint64_t> hll(14);
-
+  std::vector<uint64_t> ids;
   for(uint64_t id; data_file >> id;) {
-    hll.add(id);
+    ids.push_back(id);
   }
-  uint32_t length = hll.getSynopsisSize(Format::COMPACT_6BITS);
 
-  std::unique_ptr<char[]> byte_array(new char[length]);
-  hll.serialize(byte_array.get(), Format::COMPACT_6BITS);
-  /**
-   * Maybe we modify this as the codebase matures, but for the time being
-   * we expect his fixed length
+  for(uint8_t prec = 10; prec <= 18; ++prec) {
+    Hll<uint64_t> hll(prec);
 
-   */
-  const uint32_t ARRAY_LENGTH_6BYTES_BUCKETS_COMPRESSED_WITH_HDR = 12288U + 8;
-  EXPECT_EQ(length, ARRAY_LENGTH_6BYTES_BUCKETS_COMPRESSED_WITH_HDR);
+    for(uint64_t id: ids) {
+      hll.add(id);
+    }
+    uint32_t length = hll.getSynopsisSize(Format::COMPACT_6BITS);
 
-  Hll<uint64_t> deserialized_hll(14);
-  deserialized_hll.deserialize(byte_array.get(), Format::COMPACT_6BITS);
+    std::unique_ptr<char[]> byte_array(new char[length]);
+    hll.serialize(byte_array.get(), Format::COMPACT_6BITS);
+    /**
+     * Maybe we modify this as the codebase matures, but for the time being
+     * we expect his fixed length
 
-  EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+     */
+    const uint32_t ARRAY_LENGTH_6BYTES_BUCKETS_COMPRESSED_WITH_HDR = ((1<<prec)*6/8)+8;
+    EXPECT_EQ(length, ARRAY_LENGTH_6BYTES_BUCKETS_COMPRESSED_WITH_HDR);
+
+    Hll<uint64_t> deserialized_hll(prec);
+    deserialized_hll.deserialize(byte_array.get(), Format::COMPACT_6BITS);
+
+    EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+  }
 }
 
 /**
- * 
+ *                **************
+ *                *** 6 BITS ***
+ *                **************
  */
 TEST_F(HllTest, TestSerializeDeserialize5Bits) {
-  Hll<uint64_t> hll(14);
-
+  std::vector<uint64_t> ids;
   for(uint64_t id; data_file >> id;) {
-    hll.add(id);
+    ids.push_back(id);
   }
-  uint32_t length = hll.getSynopsisSize(Format::COMPACT_5BITS);
 
-  std::unique_ptr<char[]> byte_array(new char[length]);
-  hll.serialize(byte_array.get(), Format::COMPACT_5BITS);
-  /**
-   * Maybe we modify this as the codebase matures, but for the time being
-   * we expect his fixed length
+  for(uint8_t prec = 10; prec <= 18; ++prec) {
+    Hll<uint64_t> hll(prec);
 
-   */
-  const uint32_t ARRAY_LENGTH_5BYTES_BUCKETS_COMPRESSED_WITH_HDR = ((1<<14)*5/8)+8;
-  EXPECT_EQ(length, ARRAY_LENGTH_5BYTES_BUCKETS_COMPRESSED_WITH_HDR);
+    for(uint64_t id: ids) {
+      hll.add(id);
+    }
+    uint32_t length = hll.getSynopsisSize(Format::COMPACT_5BITS);
 
-  Hll<uint64_t> deserialized_hll(14);
-  deserialized_hll.deserialize(byte_array.get(), Format::COMPACT_5BITS);
+    std::unique_ptr<char[]> byte_array(new char[length]);
+    hll.serialize(byte_array.get(), Format::COMPACT_5BITS);
+    /**
+     * Maybe we modify this as the codebase matures, but for the time being
+     * we expect his fixed length
 
-  EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+     */
+    const uint32_t ARRAY_LENGTH_5BYTES_BUCKETS_COMPRESSED_WITH_HDR = ((1<<prec)*5/8)+8;
+    EXPECT_EQ(length, ARRAY_LENGTH_5BYTES_BUCKETS_COMPRESSED_WITH_HDR);
+
+    Hll<uint64_t> deserialized_hll(prec);
+    deserialized_hll.deserialize(byte_array.get(), Format::COMPACT_5BITS);
+
+    EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+  }
 }
 
 /**
+ *                **************
+ *                *** 5 BITS ***
+ *                **************
+ *
  * This test shares the logic with the test above.
  * However, it saves the data in a file and then reads it back in. 
  */
 TEST_F(HllTest, TestSerializeDeserialize5BitsToFile) {
-  Hll<uint64_t> hll(13);
-
+  std::vector<uint64_t> ids;
   for(uint64_t id; data_file >> id;) {
-    hll.add(id);
+    ids.push_back(id);
   }
 
-  uint32_t length = hll.getSynopsisSize(Format::COMPACT_5BITS);
-  std::unique_ptr<char[]> byte_array(new char[length]);
-  hll.serialize(byte_array.get(), Format::COMPACT_5BITS);
-  std::ofstream temp_file_out("/tmp/tmp1", std::ios::binary | std::ios::out);
-  temp_file_out.write(byte_array.get(), length);
-  temp_file_out.close();
+  for(uint8_t prec = 10; prec <= 18; ++prec) {
+    Hll<uint64_t> hll(prec);
 
-  std::ifstream temp_file_in("/tmp/tmp1", std::ios::binary | std::ios::in);
-  std::unique_ptr<char[]> byte_array2(new char[length]);
-  temp_file_in.read(byte_array2.get(), length);
-  temp_file_in.close();
-  unlink("tmp");
+    for(uint64_t id: ids) {
+      hll.add(id);
+    }
 
+    uint32_t length = hll.getSynopsisSize(Format::COMPACT_5BITS);
+    std::unique_ptr<char[]> byte_array(new char[length]);
+    hll.serialize(byte_array.get(), Format::COMPACT_5BITS);
+    std::ofstream temp_file_out("/tmp/tmp1", std::ios::binary | std::ios::out);
+    temp_file_out.write(byte_array.get(), length);
+    temp_file_out.close();
 
-  Hll<uint64_t> deserialized_hll(13);
-  deserialized_hll.deserialize(byte_array2.get(), Format::COMPACT_5BITS);
+    std::ifstream temp_file_in("/tmp/tmp1", std::ios::binary | std::ios::in);
+    std::unique_ptr<char[]> byte_array2(new char[length]);
+    temp_file_in.read(byte_array2.get(), length);
+    temp_file_in.close();
+    unlink("tmp");
 
-  EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+    Hll<uint64_t> deserialized_hll(prec);
+    deserialized_hll.deserialize(byte_array2.get(), Format::COMPACT_5BITS);
+
+    EXPECT_EQ(hll.approximateCountDistinct(), deserialized_hll.approximateCountDistinct());
+  }
 }
 
 
