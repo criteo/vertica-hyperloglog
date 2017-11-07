c4adf413054b7623a6abaefe58a14df2b69c3977
Khoa Nguyen <khoa.nguyen@gooddata.com>
2017-11-07 15:35:54 +0700

    FEATURE: Add backtrace to debug

--- SOURCES/tests/integration/04_nested_call.sql
+++ SOURCES/tests/integration/04_nested_call.sql
@@ -1,16 +1,22 @@
+-- Example table used by Average and Longest string aggregate UD functions.
+DROP IF EXISTS T;
+CREATE TABLE T (x INTEGER, y NUMERIC(5,2), z VARCHAR(10));
+COPY T FROM STDIN DELIMITER ',';
+1,1.5,'A'
+1,3.5,'A'
+2,2.0,'B'
+2,3.0,'A'
+2,2.6,'B'
+2,1.4,'A'
+3,0.5,'C'
+3,3.5,'C'
+3,1.5,'B'
+3,7.5,'B'
+4,5.5,'BC'
+4,7.5,'AB'
+\.
+
 \set ON_ERROR_STOP on
-select HllDistinctCount(synopsis USING PARAMETERS hllLeadingBits=11) as cnt
-from
-(
-  select HllCreateSynopsis(value USING PARAMETERS hlLLeadingBits=11) as synopsis
-  from
-  (
-    select 1234567890 as value
-    union
-    select 2234567890
-    union
-    select 67890
-    union
-    select 12345
-  ) as t
-) as u;
+select HllCreateSynopsis(x USING PARAMETERS hllLeadingBits=11) as synopsis
+from T
+group by z  ;
